52db182585e9c36da1fb2cfdaa4ec96b8d715c57
Alex Chang <git.noreply.ba9f11ecc34@scp.idlxb.com>
2021-06-24 22:01:31 +0000

    fixed cache control header to vercel's directive

--- pages/api/cache/v-stale.js
+++ pages/api/cache/v-stale.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  res.setHeader("Cache-Control", "max-age=10, stale-while-revalidate=59");
+  res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate=59");
   const date = new Date();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
